8f55c1c10bad2987877845337c0c64e6b7cf8a48
20345-po2 <celsocalige@gmail.com>
2022-01-22 12:31:53 +0000

    Adição dos slugs ao DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,61 +24,75 @@ class DatabaseSeeder extends Seeder
         $user = User::factory()->create();
 
         Category::create([
-            'name' => 'Bolos e Tortas Doces'
+            'name' => 'Bolos e Tortas Doces',
+            'slug' => 'bolos-tortas-doces'
         ]);
 
         Category::create([
-            'name' => 'Carnes'
+            'name' => 'Carnes',
+            'slug' => 'carnes'
         ]);
 
         Category::create([
-            'name' => 'Aves'
+            'name' => 'Aves',
+            'slug' => 'aves'
         ]);
 
         Category::create([
-            'name' => 'Peixes e Frutos do Mar'
+            'name' => 'Peixes e Frutos do Mar',
+            'slug' => 'peixes-frutos-do-mar'
         ]);
 
         Category::create([
-            'name' => 'Saladas, Molhos e Acompanhamentos'
+            'name' => 'Saladas, Molhos e Acompanhamentos',
+            'slug' => 'saladas-molhos-acompanhamentos'
         ]);
 
         $sopas = Category::create([
-            'name' => 'Sopas'
+            'name' => 'Sopas',
+            'slug' => 'sopas'
         ]);
 
         $massas = Category::create([
-            'name' => 'Massas'
+            'name' => 'Massas',
+            'slug' => 'massas'
         ]);
 
         Category::create([
-            'name' => 'Bebidas'
+            'name' => 'Bebidas',
+            'slug' => 'bebidas'
         ]);
 
         Category::create([
-            'name' => 'Doces e Sobremesas'
+            'name' => 'Doces e Sobremesas',
+            'slug' => 'doces-sobremesas'
         ]);
 
         $lanches = Category::create([
-            'name' => 'Lanches'
+            'name' => 'Lanches',
+            'slug' => 'lanches'
         ]);
 
         Category::create([
-            'name' => 'Prato Único'
+            'name' => 'Prato Único',
+            'slug' => 'prato-unico'
         ]);
 
         $light = Category::create([
-            'name' => 'Light'
+            'name' => 'Light',
+            'slug' => 'light'
         ]);
 
         Category::create([
-            'name' => 'Alimentação Saudável'
+            'name' => 'Alimentação Saudável',
+            'slug' => 'alimentacao-saudavel'
         ]);
 
         Recipe::create([
             'user_id' => $user->id,
             'category_id' => $massas->id,
             'name' => 'Pizza de frigideira com atum',
+            'slug' => 'pizza-de-frigideira-com-atum-' . $user->id,
             'servings' => 3,
             'preparationTime' => 30,
             'instructions' => 'Em uma frigideira antiaderente, coloque a massa de pizza, cubra com o molho de tomate, espalhe o atum e o queijo. Tempere com sal, pimenta e orégano. Tampe e leve ao fogo baixo por 3 minutos. Retire do fogo, espalhe a rúcula, regue com azeite e sirva em seguida.',
@@ -89,6 +103,7 @@ class DatabaseSeeder extends Seeder
             'user_id' => $user->id,
             'category_id' => $sopas->id,
             'name' => 'Frigideira de legumes ao creme',
+            'slug' => 'frigideira-de-legumes-ao-creme-' . $user->id,
             'servings' => 3,
             'preparationTime' => 30,
             'instructions' => 'Em uma frigideira antiaderente, coloque a massa de pizza, cubra com o molho de tomate, espalhe o atum e o queijo. Tempere com sal, pimenta e orégano. Tampe e leve ao fogo baixo por 3 minutos. Retire do fogo, espalhe a rúcula, regue com azeite e sirva em seguida.',
@@ -98,9 +113,9 @@ class DatabaseSeeder extends Seeder
 
         Recipe::create([
             'user_id' => $user->id,
-            'slug' => '',
             'category_id' => $light->id,
             'name' => 'Frigideira de arroz com salsicha',
+            'slug' => 'frigideira-de-arroz-com-salsicha-'. $user->id,
             'servings' => 3,
             'preparationTime' => 30,
             'instructions' => 'Em uma frigideira antiaderente, coloque a massa de pizza, cubra com o molho de tomate, espalhe o atum e o queijo. Tempere com sal, pimenta e orégano. Tampe e leve ao fogo baixo por 3 minutos. Retire do fogo, espalhe a rúcula, regue com azeite e sirva em seguida.',
@@ -112,6 +127,7 @@ class DatabaseSeeder extends Seeder
             'user_id' => $user->id,
             'category_id' => $lanches->id,
             'name' => 'Torta de frango na frigideira',
+            'slug' => 'torta-de-frango-na-frigideira-' . $user->id,
             'servings' => 3,
             'preparationTime' => 30,
             'instructions' => 'Em uma frigideira antiaderente, coloque a massa de pizza, cubra com o molho de tomate, espalhe o atum e o queijo. Tempere com sal, pimenta e orégano. Tampe e leve ao fogo baixo por 3 minutos. Retire do fogo, espalhe a rúcula, regue com azeite e sirva em seguida.',
