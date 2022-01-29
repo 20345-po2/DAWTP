ce6005db8c9a679f1c457b72334223af698509e6
20345-po2 <celsocalige@gmail.com>
2022-01-29 02:55:58 +0000

    sidebar.blade.php component

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -2,8 +2,12 @@
 <div class="card text-dark bg-light mb-3" style=" margin-left: 13.5%; margin-right: 13.5%; max-width: 50%">
     <div class="card-body">
         <h1 class="card-title">{{$heading}}</h1>
-        <div class="card-text">
-            {{$slot}}
+        <div class="flex">
+{{--            <x-sidebar class="w-48"/>--}}
+
+            <div class="card-text flex-1">
+                {{$slot}}
+            </div>
         </div>
     </div>
 </div>
